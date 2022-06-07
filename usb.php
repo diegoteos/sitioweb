919f6dfff4b059f76252672536803e55f8cf3002
diegoteos <diegoteos@hotmail.com>
2022-06-06 20:00:08 -0600

    script usb

--- usb.php
+++ usb.php
@@ -1,28 +1,10 @@
 <?php
 
-/*
-
-for ($i=9999; $i >= 0; $i--) { 
-    
-    echo $stri . $i .'<br/>';
-}
-*/
-
 $enter = "ENTER";
 $delay = "DELAY 150";
 $stri = "STRING ";
 $delays = "DELAY 50";
 
-/* for ($a=0; $a <= 9 ; $a++) { 
-    for ($b=0; $b <= 9 ; $b++) { 
-        for ($c=0; $c <= 9 ; $c++) { 
-            for ($d=0; $d <= 9 ; $d++) { 
-                echo $delay . '<br/>';
-                echo $stri . $a . $b . $c . $d . '<br/>';
-            }
-        }
-    }
-} */
 
 for ($a=8; $a <= 9 ; $a++) { 
     for ($b=0; $b <= 9 ; $b++) { 
